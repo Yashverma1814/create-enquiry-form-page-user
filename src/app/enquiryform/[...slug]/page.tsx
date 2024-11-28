07d16ee9d00298d3962d0bd98a6f3835a2ce2b23
Yash Verma <yashv1752@gmail.com>
2024-11-28 11:25:11 +0530

    add slug for route handling

---
+++ src/app/enquiryform/[...slug]/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("http://localhost:5500/enquiryform/")
+  });
+  return <div>Redirecting ....</div>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+"use client"
 import styles from "./page.module.css";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter()
+  useEffect(()=>{
+    router.push('http://localhost:5500/enquiryform/')
+  })
   return (
     <div className={styles.page}>
-      Hello User
+      Redirecting ....
     </div>
   );
 }
